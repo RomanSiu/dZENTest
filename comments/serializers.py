2d440550eed0dce504256c6ca0a0f6a4eb35b980
RomanSiu <siusiailoroman@gmail.com>
2025-06-18 23:09:15 +0300

    add XSS def

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,9 +1,13 @@
+import bleach
 from rest_framework import serializers
+
 from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
     replies = serializers.SerializerMethodField(read_only=True)
+
+    ALLOWED_TAGS = ['b', 'i', 'u', 'br']
 
     class Meta:
         model = Comment
@@ -21,12 +25,14 @@ class CommentSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         user = request.user
 
+        cleaned_text = bleach.clean(validated_data['text'], tags=self.ALLOWED_TAGS, strip=True)
+
         comment = Comment(
             user=user,
             username=user.username,
             email=user.email,
             homepage=validated_data.get('homepage'),
-            text=validated_data['text'],
+            text=cleaned_text,
             parent=validated_data.get('parent')
         )
         comment.save()
